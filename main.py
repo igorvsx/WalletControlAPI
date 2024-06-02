4beb375a7d3dbbe4f2d8ae8ae01c329ec4addddf
igorvsx <igor.19840703@gmail.com>
2024-06-02 16:40:09 +0300

    to host

--- main.py
+++ main.py
@@ -27,6 +27,6 @@ app.include_router(category_router)
 app.include_router(financial_goal_router)
 app.include_router(budget_router)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="192.168.0.115", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="192.168.0.115", port=8000)
 
